b8ec2cd9c68076f124876c6d126c6c23e3f24361
Keloran <keloran@chewedfeed.com>
2021-05-24 09:02:20 +0100

    hmm

---
+++ bugfixes.go
@@ -0,0 +1,111 @@
+package bugfixes
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime/debug"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type BugFixes struct {
+	Bug        interface{} `json:"bug"`
+	Raw        interface{} `json:"raw"`
+	BugLine    string      `json:"bug_line"`
+	File       string      `json:"file"`
+	Line       string      `json:"line"`
+	LineNumber int         `json:"line_number"`
+	Level      string      `json:"level"`
+}
+
+// New will create a new middleware handler from a http.Handler.
+func New(h http.Handler) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
+// contextKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation. This technique
+// for defining context keys was copied from Go 1.7's new use of context in net/http.
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string {
+	return "chi/middleware context value " + k.name
+}
+
+func sendToBugfixes(rvr interface{}) {
+	agentKey := os.Getenv("BUGFIXES_AGENT_KEY")
+	agentSecret := os.Getenv("BUGFIXES_AGENT_SECRET")
+	if agentKey == "" || agentSecret == "" {
+		return
+	}
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	debugStack := debug.Stack()
+	s := prettyStack{}
+	out := &bytes.Buffer{}
+	bug, err := s.bugParse(debugStack, rvr)
+	if err != nil {
+		fmt.Fprintf(out, "bugfixes: failed to parse bug: %v", err)
+		os.Stderr.Write(out.Bytes())
+		return
+	}
+
+	bugServer := "https://api.bugfix.es/bug"
+	if bugServerEnv := os.Getenv("BUGFIXES_SERVER"); bugServerEnv != "" {
+		bugServer = bugServerEnv
+	}
+
+	body, err := json.Marshal(bug)
+	if err != nil {
+		fmt.Fprintf(out, "bugfixes: failed to marshall bug: %v", err)
+		os.Stderr.Write(out.Bytes())
+		return
+	}
+	request, err := http.NewRequest("POST", bugServer, bytes.NewBuffer(body))
+	request.Header.Set("Content-type", "application/json")
+	request.Header.Set("X-API-KEY", agentKey)
+	request.Header.Set("X-API-SECRET", agentSecret)
+	if err != nil {
+		fmt.Fprintf(out, "bugfixes: failed to new request: %v", err)
+		os.Stderr.Write(out.Bytes())
+		return
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Fprintf(out, "bugfixes: failed to send bug: %v", err)
+		os.Stderr.Write(out.Bytes())
+		return
+	}
+	if err := resp.Body.Close(); err != nil {
+		fmt.Fprintf(out, "bugfixes: failed to close body: %v", err)
+		os.Stderr.Write(out.Bytes())
+		return
+	}
+}
+
+func parseBugLine(bugLine string) (string, string, int, error) {
+	i := strings.Index(bugLine, ":")
+	j := strings.Index(bugLine, " ")
+	file := bugLine[:i]
+	lne := bugLine[i+1 : j]
+	line, err := strconv.Atoi(lne)
+	if err != nil {
+		return file, lne, 0, fmt.Errorf("failed to convert line number: %w", err)
+	}
+
+	return file, lne, line, nil
+}
