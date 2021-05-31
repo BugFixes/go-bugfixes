647c795ce8ee6f6281f2ee2dbf5938b90dd88955
Keloran <keloran@chewedfeed.com>
2021-05-31 12:40:13 +0100

    added output if it tries to send but missing the keys

--- logs/logging.go
+++ logs/logging.go
@@ -47,6 +47,17 @@ func (b BugFixesLog) sendLog() {
 		bugServer = bugServerEnv
 	}
 
+  if agentKey == "" || agentSecret == "" {
+    fmt.Printf("cant send to server till you have created an agent and set the keys\n")
+    if agentKey == "" {
+      fmt.Printf("env: BUGFIXES_AGENT_KEY missing\n")
+    }
+    if agentSecret == "" {
+      fmt.Printf("env: BUGFIXES_AGENT_SECRET missing\n")
+    }
+    return
+  }
+
 	body, err := json.Marshal(b)
 	if err != nil {
 		fmt.Printf("bugfixes sendLog marshal: %+v", err)
