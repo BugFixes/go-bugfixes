9acddfb3b5f215d5840161eef75e429a4a818561
Keloran <keloran@develbox.info>
2024-05-01 21:51:33 +0100

    feat: Added CORS middleware functionality
    - Created methods to add allowed origins, headers, and methods
    - Implemented logic to check if origin is allowed and set appropriate headers
    - Added CORS middleware to handle CORS requests

---
+++ middleware/cors.go
@@ -0,0 +1,66 @@
+package middleware
+
+import (
+  "net/http"
+  "strings"
+)
+
+// Origins
+func (s *System) AddAllowedOrigins(origins ...string) {
+	s.AllowedOrigins = append(s.AllowedOrigins, origins...)
+}
+
+// Headers
+func (s *System) AddAllowedHeaders(headers ...string) {
+	s.AllowedHeaders = append(s.AllowedHeaders, headers...)
+}
+func (s *System) getAllowedHeaders() string {
+	standardAllowed := []string {
+		"Accept",
+		"Content-Type",
+	}
+	
+	allowedHeaders := append(standardAllowed, s.AllowedHeaders...)
+	return strings.Join(allowedHeaders, ", ")
+}
+
+// Methods
+func (s *System) AddAllowedMethods(methods ...string) {
+	s.AllowedMethods = append(s.AllowedMethods, methods...)
+}
+func (s *System) getAllowedMethods() string {
+	return strings.Join(s.AllowedMethods, ", ")
+}
+
+
+func (s *System) CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		originalOrigin := r.Header.Get("Origin")
+
+		isAllowed := false
+		for _, origin := range s.AllowedOrigins {
+			if origin == originalOrigin {
+				isAllowed = true
+				break
+			}
+		}
+
+		if !isAllowed {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", originalOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", s.getAllowedMethods())
+		w.Header().Set("Access-Control-Allow-Headers", s.getAllowedHeaders())
+		w.Header().Set("Access-Control-Max-Age", "86400")
+		w.Header().Set("Var", "Origin")
+		
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		
+		next.ServeHTTP(w, r)
+	})
+}
